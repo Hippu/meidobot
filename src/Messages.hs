3bd76231dc450ae438934ea4701807e9a40c80e2
Teemu Heinämäki <teemu.heinamaki@gmail.com>
2019-01-10 10:51:46 +0200

    The most important feature

--- src/Messages.hs
+++ src/Messages.hs
@@ -5,11 +5,12 @@ module Messages
 
 import qualified Data.Text as T
 import Discord
-
+import qualified Data.List as List
 
 responseChooser :: T.Text -> Maybe T.Text
 responseChooser t
     | T.isPrefixOf "moi" t = Just "Moi!"
+    | paskaMaailma t = Just "http://gifs.hippuu.fi/g/112197.gif"
     | otherwise = Nothing
 
 messages :: User -> Message -> Maybe T.Text
@@ -18,3 +19,7 @@ messages botUser message =
         responseChooser $ T.toLower $ messageText message
     else
         Nothing
+
+paskaMaailma :: T.Text -> Bool
+paskaMaailma t =
+    (List.elem "paska" $ T.words t) && (List.elem "maailma" $ T.words t)
